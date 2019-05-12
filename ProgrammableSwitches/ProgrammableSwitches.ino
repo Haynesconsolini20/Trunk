2a6115cb438459273f62690d228faf39312f2fe2
Sam Haynes <haynesconsolini@gmail.com>
2019-05-12 12:00:58 -0700

    Comments and minor fixes on programmable switches

--- ProgrammableSwitches/ProgrammableSwitches.ino
+++ ProgrammableSwitches/ProgrammableSwitches.ino
@@ -4,20 +4,28 @@
  * Hold button for 2 seconds, switch to desired config, then hold again to reprogram
  */
 
-
-//Touch pins
+/**********************************************************************
+ * PINS
+ **********************************************************************/
 int pins[9] = {3,4,5,6,7,8,9,10,11};
-int correct[9] = {1,1,1,1,1,1,1,1,1};
-
-//Output device
 #define outputDevice 12
-int counter = 0;
 #define wrongSound 13
 #define inputSound 14
 #define correctSound 15
 #define confirmInputs 16
 
+/**********************************************************************
+ * GLOBALS
+ * correct - the correct position for all 9 switches
+ * SOUND_DELAY - sound playing duration
+ **********************************************************************/
+int correct[9] = {1,1,1,1,1,1,1,1,1};
+#define SOUND_DELAY 200
 
+/**********************************************************************
+ * CHECK INPUTS
+ * 
+ **********************************************************************/
 bool checkInputs() {
   for (int i = 0; i < 9; i++) {
     if (digitalRead(pins[i]) != correct[i]) {
@@ -26,6 +34,13 @@ bool checkInputs() {
   }
   return true;
 }
+
+/**********************************************************************
+ * REPROGRAM SWITCHES
+ * Plays the win sound to confirm you are in the sequence. Waits
+ * for the button to be held for 2 seconds and then writes the current
+ * switch states as the correct states. Ends with win sound to confirm
+ **********************************************************************/
 void reprogramSwitches() {
   playSound(correctSound);
   while (true) {
@@ -42,21 +57,35 @@ void reprogramSwitches() {
   playSound(correctSound);
 }
 
+/**********************************************************************
+ * PLAY SOUND
+ * Plays a sound for SOUND_DELAY milliseconds
+ **********************************************************************/
 void playSound(int soundPin) {
   digitalWrite(soundPin, HIGH);
-  delay(200);
+  delay(SOUND_DELAY);
   digitalWrite(soundPin, LOW);
 }
 
+/**********************************************************************
+ * CHECK WIN
+ * Returns true if the current state of the switches matches the correct
+ * array
+ **********************************************************************/
 bool checkWin() {
   for (int i = 0; i < 9; i++) {
-    if (correct[i] != pressed[i]) {
+    if (correct[i] != digitalRead(pins[i])) {
       return false;
     }
   }
   return true;
 }
 
+/**********************************************************************
+ * RESET SEQUENCE
+ * If all switches are set to high, reset. If the button is held for
+ * 2 seconds, begin the reprogramming sequence
+ **********************************************************************/
 void resetSequence() {
   while(true) {
     bool reset = true;
@@ -80,6 +109,10 @@ void resetSequence() {
   }
 }
 
+/**********************************************************************
+ * SETUP
+ * Initialize pins and initial states
+ **********************************************************************/
 void setup() {
   for (int i=0; i < 9; i++) {
     pinMode(pins[i], INPUT);
@@ -99,10 +132,12 @@ void setup() {
   
   pinMode(correctSound, OUTPUT);
   digitalWrite(correctSound, LOW);
-  
-
 }
 
+/**********************************************************************
+ * LOOP
+ * General game loop
+ **********************************************************************/
 void loop() {
   //Loop through the 4 inputs
   if (digitalRead(confirmInputs)) {
