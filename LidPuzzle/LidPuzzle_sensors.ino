04be4c11df3ebf2713e276b2207388a21e9c97ce
Sam Haynes <haynesconsolini@gmail.com>
2019-05-23 18:37:41 -0700

    Branched sensor beta into different file, put main file to buttons

--- LidPuzzle/LidPuzzle_sensors.ino
+++ LidPuzzle/LidPuzzle_sensors.ino
@@ -1,4 +1,31 @@
 /**********************************************************************
+ * CAPACATIVE TOUCH SETUP
+ **********************************************************************/
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_CAP1188.h>
+
+// Reset Pin is used for I2C or SPI
+#define CAP1188_RESET  9
+
+// CS pin is used for software or hardware SPI
+#define CAP1188_CS  10
+
+// These are defined for software SPI, for hardware SPI, check your 
+// board's SPI pins in the Arduino documentation
+#define CAP1188_MOSI  11
+#define CAP1188_MISO  12
+#define CAP1188_CLK  13
+
+// For I2C, connect SDA to your Arduino's SDA pin, SCL to SCL pin
+// On UNO/Duemilanove/etc, SDA == Analog 4, SCL == Analog 5
+// On Leonardo/Micro, SDA == Digital 2, SCL == Digital 3
+// On Mega/ADK/Due, SDA == Digital 20, SCL == Digital 21
+
+// Use I2C, no reset pin!
+Adafruit_CAP1188 cap = Adafruit_CAP1188();
+
+/**********************************************************************
  * PINS
  **********************************************************************/
 int pins[4] = {5,6,7,8};
@@ -48,12 +75,17 @@ bool checkWin() {
 void resetSequence() {
   while(true) {
     int resetCount = 0;
-    
-    for (int i=0; i<4; i++) {
-        if (digitalRead(pins[i])) {
-          resetCount++;
-          playSound(inputSound);
-        }
+    uint8_t touched = cap.touched();
+    if (touched == 0) {
+      // No touch detected
+      return;
+    }
+    for (uint8_t i=0; i<8; i++) {
+      if (touched & (1 << i)) {
+        Serial.print("C"); Serial.print(i+1 / 2); Serial.print("\t");
+        resetCount++;
+        playSound(inputSound);
+      }
     }
     if (resetCount == 4) break;
   }
@@ -64,10 +96,6 @@ void resetSequence() {
  * Set up the pins and initial values
  **********************************************************************/
 void setup() {
-  for (int i = 0; i < 4; i++) {
-    pinMode(pins[i], INPUT);
-  }
-  
   pinMode(outputDevice, OUTPUT);
   digitalWrite(outputDevice, HIGH);
   
@@ -80,6 +108,17 @@ void setup() {
   pinMode(correctSound, OUTPUT);
   digitalWrite(correctSound, HIGH);
 
+  Serial.begin(9600);
+  Serial.println("CAP1188 test!");
+
+  // Initialize the sensor, if using i2c you can pass in the i2c address
+  // if (!cap.begin(0x28)) {
+  if (!cap.begin()) {
+    Serial.println("CAP1188 not found");
+    while (1);
+  }
+  Serial.println("CAP1188 found!");
+
 }
 
 /**********************************************************************
@@ -87,9 +126,15 @@ void setup() {
  * General game loop
  **********************************************************************/
 void loop() {
-  for (int i=0; i < 4; i++) {
-    if (digitalRead(pins[i]) {
-      pressed[counter] = pins[i];
+  uint8_t touched = cap.touched();
+  if (touched == 0) {
+    // No touch detected
+    return;
+  }
+  for (uint8_t i=0; i<8; i++) {
+    if (touched & (1 << i)) {
+      Serial.print("C"); Serial.print((i+1) /2); Serial.println("\t");
+      pressed[counter] = (i+1) / 2;
       counter++;
       playSound(inputSound);
     }
@@ -97,11 +142,13 @@ void loop() {
   // If we've gotten 4 inputs, check the win state
   if (counter == 4) {
     if (checkWin()) {
+      Serial.println("boiiiii");
       playSound(correctSound);
       counter = 0; 
       resetSequence();
     }
     else {
+      Serial.println("boy....");
       playSound(wrongSound);
       counter = 0;
     }
